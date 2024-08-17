d8591cd7ff5d7e5b24a086e11281e174140e49ed
ernkerr <erin.kerr17@gmail.com>
2024-08-16 18:02:08 -0700

    add py file

---
+++ 100 Days of Code/higher_or_lower.py
@@ -0,0 +1,39 @@
+guiness_data = [
+    {
+        "Name": "tree",
+        "Height": 379.7,
+        "Unit": "feet",
+        "Fun Fact": "The world's tallest tree is a coast redwood (Sequoia sempervirens) named Hyperion. It stands at approximately 379.7 feet (115.7 meters) tall.",
+    },
+    {
+        "Name": "roller coaster",
+        "Height": 456,
+        "Unit": "feet",
+        "Fun Fact": "The tallest roller coaster in the world is the Kingda Ka at Six Flags Great Adventure in New Jersey, USA. It stands at 456 feet (139 meters) tall."
+    },
+
+]
+
+normal_data = [
+    {
+        "Name": "Statue of Liberty",
+        "Height": 305,
+        "Unit": "feet",
+        "Fun Fact": "The Statue of Liberty is approximately 305 feet (93 meters) tall from the ground to the tip of the torch."
+    },
+    {
+        "Name": "Eiffel Tower",
+        "Height": 1083,
+        "Unit": "feet",
+        "Fun Fact": "The Eiffel Tower in Paris, France, is 1,083 feet (330 meters) tall, including its antenna. The height to the top of the roof is 984 feet (300 meters)."
+    },
+    {
+        "Name": "",
+        "Height": "",
+        "Unit": "",
+        "Fun Fact": ""
+    },
+]
+
+# These are dictionaries nested in a list
+# print(programming_dictionary["Bug"])
