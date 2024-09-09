97183f9da08922555049170937c9c440b5908589
ernkerr <erin.kerr17@gmail.com>
2024-09-09 13:14:19 -0700

    add js to the repo

---
+++ main.js
@@ -0,0 +1,16 @@
+// comparison operators 
+if (true) {
+    console.log('This message will print!');
+  }
+  // Prints: This message will print!
+  
+if (stopLight === 'green' && pedestrians === 0) {
+    console.log('Go!');
+  } else {
+    console.log('Stop');
+  }
+if (day === 'Saturday' || day === 'Sunday') {
+    console.log('Enjoy the weekend!');
+  } else {
+    console.log('Do some work.');
+  }
