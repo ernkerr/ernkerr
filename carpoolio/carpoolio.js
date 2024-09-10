e40d3c82efa0fe5858d18f910253b4c9e4673f56
ernkerr <erin.kerr17@gmail.com>
2024-09-10 16:47:31 -0700

    carpoolio

---
+++ carpoolio/carpoolio.js
@@ -0,0 +1,28 @@
+// onboarding 
+
+// Step 1: Obtain Name (use default parameters ?)
+
+// Step 2: Are you the driver Y/N
+// y: make name driver's name 
+// n: who's driving
+
+// Step 3: How many seats are in your car? 
+// scrolly thing 
+
+// Step 4: Add riders (get shareable link) 
+
+
+
+
+
+
+
+
+
+// if user does not enter a name, use a short-circuit evaluation 
+
+let username = '';
+let defaultName = username || 'Stranger';
+
+console.log(`Welcome, ${defaultName}`); // Prints: Stranger since username is empty 
+
