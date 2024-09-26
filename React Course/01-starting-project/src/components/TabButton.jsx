e5821230bab3d2674f2bbe07c2ee6c774dbcf963
ernkerr <erin.kerr17@gmail.com>
2024-09-26 14:50:27 -0700

    add tab buttons

---
+++ React Course/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,38 @@
+// export default function TabButton(props) {
+//     return <li><button>{props.children}</button></li>;
+// };
+
+
+// or using destructuring 
+
+
+export default function TabButton({children}) {
+    return (
+      <li>
+        <button>{children}</button>
+      </li>
+      );
+};
+
+// This was of building components where your components can wrap other components
+// or other content is called component composition 
+
+// also known as "using children" 
+// for components that take a single pice of renderable content
+// esp when passing JSX code as a value to another component 
+
+
+
+
+// vs. 
+
+
+
+// using attributes
+// if you have multiple but smaller peices of information that must be passed to a component 
+
+/* <TabButton label="Components"></TabButton>
+
+function TabButton({label}) {
+  return <button>{label}</button>;
+} */
