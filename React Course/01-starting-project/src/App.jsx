6ba9bde201aa11fa589ce879011802a06150c70b
ernkerr <erin.kerr17@gmail.com>
2024-09-27 16:56:27 -0700

    add state for tab button

--- React Course/01-starting-project/src/App.jsx
+++ React Course/01-starting-project/src/App.jsx
@@ -15,6 +15,21 @@ const [ selectedTopic, setSelectedTopic ] = useState();
     // console.log(selectedTopic);
   }
 
+  let tabContent = <p>Please select a topic.</p> 
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div> 
+    );
+  }
+
+
   return (
     <div>
       <Header />
@@ -40,17 +55,7 @@ const [ selectedTopic, setSelectedTopic ] = useState();
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          
-        {!selectedTopic ? (
-          <p>Please select a topic.</p> 
-        ) : (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div> )}
+          {tabContent}
         </section>
       </main>
     </div>
