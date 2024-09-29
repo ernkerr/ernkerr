2f798d09879ae92aa56d3672ec0ff9afbc5d2c43
ernkerr <erin.kerr17@gmail.com>
2024-09-29 12:16:17 -0700

    header and backround formatting

--- carpoolio/src/components/Header/Header.jsx
+++ carpoolio/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
       <header>
         <h2>carpoolio </h2>
+        <h4>because it's not just about the destination</h4>
       </header>
     );
 }
