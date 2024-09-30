abcf9b60ae4b3017ad1b026155ce0db267a59187
ernkerr <erin.kerr17@gmail.com>
2024-09-30 15:50:16 -0700

    add username component for carpoolio

--- carpoolio/src/pages/NewTripPage.jsx
+++ carpoolio/src/pages/NewTripPage.jsx
@@ -2,8 +2,6 @@ import Header from "../components/Header/Header"
 import GetUsername from "../components/GetUsername/GetUsername";
 
 export default function NewTripPage() {
-    let username = '';
-    let defaultName = username || 'Stranger'; //short-circuit evaluation
 
     return(
 
