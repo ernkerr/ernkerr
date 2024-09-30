0326fd1ddf2b024128e7ac60817ae79dca0bd98d
ernkerr <erin.kerr17@gmail.com>
2024-09-30 12:50:55 -0700

    build reusable player component

--- React - tic tac toe/07-tic-tac-toe-starting-project/src/App.jsx
+++ React - tic tac toe/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,17 @@
+import Player from './components/Player.jsx'
+
 
 function App() {
   return (
-<p> coming soon </p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol= "X" /> 
+          <Player name="Player 2" symbol= "O" />
+        </ol>
+        
+      </div>
+    </main>
   )
 }
 
