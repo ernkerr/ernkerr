bc3d4d5951b25b8a1be836a82c6b244571a3ded6
ernkerr <erin.kerr17@gmail.com>
2024-10-05 00:18:50 -0700

    home page background

--- carpoolio/src/pages/HomePage.jsx
+++ carpoolio/src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
+import Header from "../components/Header/Header.jsx";
+import NewTripButton from "../components/NewTripButton/NewTripButton.jsx";
+import MovingCar from "../components/MovingCar/MovingCar.jsx";
+import StickyNavBar from "../components/StickyNavBar/StickyNavBar.jsx";
+import bluegoo from "../assets/bluegoo.gif";
+import "./HomePage.css"; // Ensure this is at the top of your file
 
-import Header from '../components/Header/Header.jsx'
-import NewTripButton from '../components/NewTripButton/NewTripButton.jsx';
-import MovingCar from '../components/MovingCar/MovingCar.jsx';
-import StickyNavBar from '../components/StickyNavBar/StickyNavBar.jsx';
-
-function HomePage(){
-    
-    return(
-    <div>
-        <StickyNavBar showCreateTrip />
-        <Header />
-        <NewTripButton>Try It Out</NewTripButton>
-        <MovingCar /> 
+function HomePage() {
+  return (
+    <div className="home-page">
+      <StickyNavBar showCreateTrip />
+      <Header />
+      <NewTripButton>Try It Out</NewTripButton>
+      <MovingCar />
     </div>
-    );
+  );
 }
 export default HomePage;
