91c1accd8a0a9a831a3e3b7c8a82e40186608112
ernkerr <erin.kerr17@gmail.com>
2024-10-08 22:09:39 -0700

    custom car styling

--- carpoolio/src/components/CustomizeCar/NumSeats.jsx
+++ carpoolio/src/components/CustomizeCar/NumSeats.jsx
@@ -1,28 +1,26 @@
-import React, { useState } from 'react';
-import './CustomizeCar.css'
+import React, { useState } from "react";
+import "./CustomizeCar.css";
 
-export default function NumSeats(){
+export default function NumSeats() {
+  const [numSeats, setNumSeats] = useState(0);
 
-    const [numSeats, setNumSeats] = useState(0)
+  const handleSliderChange = (event) => {
+    setNumSeats(event.target.value);
+  };
 
-    const handleSliderChange = (event) => {
-        setNumSeats(event.target.value);
-    };
+  return (
+    <>
+      <h4 className="form-question">number of avaliable seats: {numSeats}</h4>
 
-    return (
-        <>
-            <h4 id="form-question">number of avaliable seats</h4>
-
-                <input 
-                 type="range" 
-                 min="0" 
-                 max="10" 
-                 value={numSeats}
-                 onChange={handleSliderChange}
-                 className="slider" 
-                 id="myRange" />
-
-            <h4 id="form-question">num seats: {numSeats}</h4>
-        </>
-    );
+      <input
+        type="range"
+        min="0"
+        max="10"
+        value={numSeats}
+        onChange={handleSliderChange}
+        className="slider"
+        id="myRange"
+      />
+    </>
+  );
 }
