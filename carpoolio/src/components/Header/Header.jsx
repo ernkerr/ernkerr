0fe16f402a666745acc3f5a61dbb4651858257f5
ernkerr <erin.kerr17@gmail.com>
2024-10-08 22:08:11 -0700

    styling the carpoolio logo

--- carpoolio/src/components/Header/Header.jsx
+++ carpoolio/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import './Header.css'
-import CarpoolioSpinner from '../CarpoolioSpinner/CarpoolioSpinner';
+import "./Header.css";
+import CarpoolioSpinner from "../CarpoolioSpinner/CarpoolioSpinner";
 
 export default function Header() {
-    return (
-      <header>
-        <h1>carpoolio </h1>
-        <h4 id='destination-message'>because it's not <em>just</em> about the destination</h4>
-        <CarpoolioSpinner />
-        <h4 id='carpooling-message'> organized carpooling for your next trip </h4>
-      </header>
-    );
+  return (
+    <header>
+      <h1 className="font-face">carpoolio </h1>
+      <h4 id="destination-message">
+        because it's not <em>just</em> about the destination
+      </h4>
+      <CarpoolioSpinner />
+      <h4 id="carpooling-message"> organized carpooling for your next trip </h4>
+    </header>
+  );
 }
