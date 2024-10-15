028670df01ffa17d17c4cc2a1957f4d5989251da
ernkerr <erin.kerr17@gmail.com>
2024-10-15 15:49:26 -0700

    egghead course: rendering images

--- egghead-beginners-guide-nextjs/exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ egghead-beginners-guide-nextjs/exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     <div>
       <h2>Hello 👋</h2>
       <p>My name is Lazar Nikolov</p>
+      {/* <img src="/lazar.png" /> */}
       {/* ✍️ add the "lazar.png" image here */}
     </div>
   )
--- egghead-beginners-guide-nextjs/exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ egghead-beginners-guide-nextjs/exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,12 @@
 // ✍️ import the Image component from 'next/image'
+import Image from 'next/image'
 
 // ✍️ import "lazar.png" statically
+import lazar from '../lazar.png'
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return <Image src={lazar} alt="Lazar Nikolov" />
+  /* ✍️ add the lazar image here */
 }
 
 export default Home
