2520386c33693a8e4f2fe47fd92b08cf6b6283c0
ernkerr <erin.kerr17@gmail.com>
2024-10-15 15:59:50 -0700

    egghead course: page specific metadata

--- egghead-beginners-guide-nextjs/exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
+++ egghead-beginners-guide-nextjs/exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
@@ -1,9 +1,17 @@
 // ✍️ import Head from 'next/head'
+import Head from 'next/head'
 import { ChakraProvider, Heading } from '@chakra-ui/react'
 
 const Home = () => {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Homepage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Head>
+        <meta name="og:title" content="My Next.js course" key="title" />
+      </Head>
       {/* ✍️ add title and description using the Head component */}
       <Heading w="full" textAlign="center">
         This is our homepage content!
