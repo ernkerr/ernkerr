409b2af724e65752e37b7a65782b4d8bc8eb2e86
ernkerr <erin.kerr17@gmail.com>
2024-10-30 12:58:47 -0700

    description sizing

--- Carpoolio/src/components/CustomizeTrip/Description/Description.jsx
+++ Carpoolio/src/components/CustomizeTrip/Description/Description.jsx
@@ -1,27 +1,33 @@
+import "./Description.css";
+
 export default function Description({
   formData,
   setFormData,
   isPreviewingTrip,
 }) {
+  const handleInput = (event) => {
+    event.target.style.height = "4dvh";
+    event.target.style.height = `${event.target.scrollHeight}px`;
+    setFormData({ ...formData, tripDescription: event.target.value });
+  };
+
   return (
     <>
       {!isPreviewingTrip || formData.tripDescription ? (
-        <input
-          className="customize-trip-btns"
+        <textarea
+          id="description-textarea"
+          placeholder={formData?.tripDescription || "Trip Description"}
+          value={formData.tripDescription || ""}
+          onChange={handleInput}
           style={{
+            height: "4dvh",
             background: formData?.tripBackground?.scrim || "transparent",
             border: isPreviewingTrip
               ? "2px solid transparent"
               : "2px solid rgba(255, 255, 255, 0.182)",
             borderRadius: isPreviewingTrip ? "0" : "5px",
           }}
-          type="text"
-          required
-          placeholder={formData?.tripDescription || "Trip Description"}
-          onChange={(event) => {
-            setFormData({ ...formData, tripDescription: event.target.value });
-          }}
-        ></input>
+        ></textarea>
       ) : null}
     </>
   );
