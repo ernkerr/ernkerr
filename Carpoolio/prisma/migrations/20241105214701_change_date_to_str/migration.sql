3d3e35fdfe563a36b81943c2d538237c6de9d53f
ernkerr <erin.kerr17@gmail.com>
2024-11-05 14:11:42 -0800

    date as str

---
+++ Carpoolio/prisma/migrations/20241105214701_change_date_to_str/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ALTER COLUMN "tripDate" SET DATA TYPE TEXT;
--- Carpoolio/src/components/CustomizeTrip/DateSelector/DateSelector.jsx
+++ Carpoolio/src/components/CustomizeTrip/DateSelector/DateSelector.jsx
@@ -39,11 +39,11 @@ export default function DateSelector({
         day: "numeric",
       });
 
-      // Convert selected date to ISO-8601 format for consistency with API
-      const isoFormattedDate = date.toISOString().split("T")[0]; // "YYYY-MM-DD"
+      // // Convert selected date to ISO-8601 format for consistency with API
+      // const isoFormattedDate = date.toISOString().split("T")[0]; // "YYYY-MM-DD"
 
       // Update formData with the selected date
-      setFormData({ ...formData, tripDate: isoFormattedDate });
+      setFormData({ ...formData, tripDate: formattedDate });
       setIsCalendarVisible(false); // Close calendar after date is selected
     }
   };
