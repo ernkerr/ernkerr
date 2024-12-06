b341f19a8779cc9ff701bb98cc540dd4865a2bf1
Erin Kerr <erin.kerr17@gmail.com>
2024-12-05 23:16:42 -0800

    add href to logo

--- carpoolio/src/components/Header/Header.jsx
+++ carpoolio/src/components/Header/Header.jsx
@@ -4,9 +4,13 @@ import logo from "../../assets/img/logo2.png";
 
 export default function Header() {
   return (
-    <header>
-      <img className="logo" src={logo} alt="Carpoolio Logo" />
-      {/* <p className="carpoolio">carpoolio </p> */}
-    </header>
+    <>
+      <header>
+        <a id="logo" href="/#home">
+          <img className="logo" src={logo} alt="Carpoolio Logo" />
+        </a>
+        {/* <p className="carpoolio">carpoolio </p> */}
+      </header>
+    </>
   );
 }
