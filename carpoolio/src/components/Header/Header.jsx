714c90aa6572591094972a003e0a77d25191a5b0
Erin Kerr <erin.kerr17@gmail.com>
2024-12-05 23:21:30 -0800

    link instead of a

--- carpoolio/src/components/Header/Header.jsx
+++ carpoolio/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import "./Header.css";
-
+import { Link } from "react-router-dom";
 import logo from "../../assets/img/logo2.png";
 
 export default function Header() {
   return (
     <>
       <header>
-        <a id="logo" href="/#home">
+        <Link id="logo" to="/">
           <img className="logo" src={logo} alt="Carpoolio Logo" />
-        </a>
+        </Link>
         {/* <p className="carpoolio">carpoolio </p> */}
       </header>
     </>
