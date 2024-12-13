89ab62b038c298fd6b8d88ba9bcfe6fc9a9d7e43
Erin Kerr <erin.kerr17@gmail.com>
2024-12-12 23:56:51 -0800

    overlays

--- Carpoolio/src/components/TripOverlay.jsx
+++ Carpoolio/src/components/TripOverlay.jsx
@@ -11,6 +11,21 @@ import squiddy from "../assets/gifs/squiddy.gif";
 import rain from "../assets/gifs/rain.gif";
 import none from "../assets/gifs/none.png";
 
+import stars from "../assets/gifs/stars.gif";
+import twinklingStars from "../assets/gifs/twinklingStars.gif";
+// import snowflake from "../assets/gifs/snowflake.gif";
+// import clouds1 from "../assets/gifs/clouds1.gif";
+// import stars2 from "../assets/gifs/stars2.gif";
+import sparklers from "../assets/gifs/sparklers.gif";
+import goldfish from "../assets/gifs/goldfish.gif";
+// import glitter from "../assets/gifs/glitter.gif";
+import rain2 from "../assets/gifs/snow2.gif";
+// import lightning from "../assets/gifs/lightning.gif";
+// import snowflakes from "../assets/gifs/snowflakes.gif";
+
+// import  from "../assets/gifs/.gif";
+// import  from "../assets/gifs/.gif";
+
 // make sure this is saving to formData (!)
 
 export default function TripOverlay({ isPreviewingTrip }) {
@@ -38,6 +53,20 @@ export default function TripOverlay({ isPreviewingTrip }) {
     // { name: "squiddy ", path: squiddy },
     { name: "rain", path: rain },
     // { name: "clouds", path: clouds },
+
+    { name: "stars", path: stars },
+    { name: "twinklingStars", path: twinklingStars },
+    // { name: "snowflakes", path: snowflakes },
+    // { name: "clouds1", path: clouds1 },
+    // { name: "stars2", path: stars2 },
+    { name: "sparklers", path: sparklers },
+    { name: "goldfish", path: goldfish },
+    // { name: "glitter", path: glitter },
+    { name: "rain2", path: rain2 },
+    // { name: "lightning", path: lightning },
+    // { name: "snowflakes", path: snowflakes },
+
+    // { name: "", path:  },
   ];
 
   return (
