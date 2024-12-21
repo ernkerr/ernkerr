e492eadcffd786f4492bac64095ad0ea88575785
Erin Kerr <erin.kerr17@gmail.com>
2024-12-21 12:15:27 -0800

    F to C converter

---
+++ Side Projects/TemperatureConverter.js
@@ -0,0 +1,14 @@
+// get input from user ( either F or C )
+
+// F to C
+let tempInF = 0; // change to input from user
+
+let OutputTempInC = (5 / 9) * (tempInF - 32);
+
+// C to F
+let tempInC = 32; // change to input from user
+
+let OutputTempInF = (5 / 9) * (tempInC + 32);
+
+// if input = F then fahrenheitToCelcius
+// else if input = C then celciusToFahrenheit
